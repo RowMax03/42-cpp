6582f31e457f4de9f322f88b9e9b60e24ae85f4c
Max Reidenbach <mreidenb@student.42heilbronn.de>
2024-05-11 14:54:27 +0200

    Changed approach to mutiple template functions instead of 1 general for 08 | ex00, working

--- 08/ex00/include/easyfind.hpp
+++ 08/ex00/include/easyfind.hpp
@@ -6,73 +6,40 @@
 /*   By: mreidenb <mreidenb@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:53:55 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/10 18:04:09 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/11 14:22:56 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Helper struct to check for find member function
+#pragma once
+
+#include <algorithm>
+
+// For containers that have an iterator
 template <typename T>
-struct has_find
-{
-private:
-	typedef char yes[1];
-	typedef char no[2];
+typename T::iterator easyfind(T &container, int value) {
+	return (std::find(container.begin(), container.end(), value));
+}
 
-	template <typename C>
-	static yes& test( typeof(&C::find) ) ;
-
-	template <typename C>
-	static no& test(...);
-
-public:
-	enum { value = sizeof(test<T>(0)) == sizeof(yes) };
-};
-
-// Functor to compare the first member of a pair with a given value
-struct CompareFirst
-{
-	int value;
-	CompareFirst(int val) : value(val) {}
-	bool operator()(const std::pair<const int, int>& pair)
-	{
-		return pair.first == value;
+// For containers with key-value pairs
+template <typename T_key, typename T_value>
+typename std::map<T_key, T_value>::iterator easyfind(std::map<T_key, T_value> &container, int value) {
+	typename std::map<T_key, T_value>::iterator it = container.begin();
+	while (it != container.end()) {
+		if (it->second == value)
+			return (it);
+		it++;
 	}
-};
+	return (container.end());
+}
 
-// Base template for easyfind
-template <typename T, bool hasFind>
-struct easyfind_impl
-{
-	static typename T::iterator find(T &container, int value)
-	{
-		CompareFirst comp(value);
-		typename T::iterator it = std::find_if(container.begin(), container.end(), comp);
-		if (it == container.end())
-		{
-			throw std::exception();
-		}
-		return it;
+// For containers with muti key-value pairs
+template <typename T_key, typename T_value>
+typename std::multimap<T_key, T_value>::iterator easyfind(std::multimap<T_key, T_value> &container, int value) {
+	typename std::multimap<T_key, T_value>::iterator it = container.begin();
+	while (it != container.end()) {
+		if (it->second == value)
+			return (it);
+		it++;
 	}
-};
-
-// Specialization for containers with a find member function
-template <typename T>
-struct easyfind_impl<T, true>
-{
-	static typename T::iterator find(T &container, int value)
-	{
-		typename T::iterator it = container.find(value);
-		if (it == container.end())
-		{
-			throw std::exception();
-		}
-		return it;
-	}
-};
-
-// The actual easyfind function that delegates to the correct implementation
-template <typename T>
-typename T::iterator easyfind(T &container, int value)
-{
-	return easyfind_impl<T, has_find<T>::value>::find(container, value);
+	return (container.end());
 }
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,28 +6,84 @@
 /*   By: mreidenb <mreidenb@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:33:08 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/10 18:11:59 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/11 14:50:58 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include <map>
 #include <iostream>
+#include <list>
+#include <deque>
+#include <set>
 #include "easyfind.hpp"
 
 int main()
 {
+	// Vector
+	std::cout << "--- Vector ---" << std::endl;
 	std::vector<int> v;
+	v.push_back(42);
+	int search_v = 42;
+	std::vector<int>::iterator it = easyfind(v, search_v);
+	if (it == v.end())
+		std::cout << "Did not find "<< search_v << std::endl;
+	else
+		std::cout << "Found: " << *it << std::endl;
+
+	// List
+	std::cout << "--- List ---" << std::endl;
+	std::list<int> l;
+	l.push_back(33);
+	int search_l = 33;
+	std::list<int>::iterator it1 = easyfind(l, search_l);
+	if (it1 == l.end())
+		std::cout << "Did not find "<< search_l << std::endl;
+	else
+		std::cout << "Found: " << *it1 << std::endl;
+	
+	// Deque
+	std::cout << "--- Deque ---" << std::endl;
+	std::deque<int> d;
+	d.push_back(44);
+	int search_d = 44;
+	std::deque<int>::iterator it3 = easyfind(d, search_d);
+	if (it3 == d.end())
+		std::cout << "Did not find "<< search_d << std::endl;
+	else
+		std::cout << "Found: " << *it3 << std::endl;
+	
+	// Map
+	std::cout << "--- Map ---" << std::endl;
 	std::map<int, int> m;
 	m[42] = 3;
-	v.push_back(42);
-	try
-	{	
-		std::vector<int>::iterator it = easyfind(v, 42);
-		std::cout << "Found: " << *it << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	int search_m = 3;
+	std::map<int, int>::iterator it2 = easyfind(m, search_m);
+	if (it2 == m.end())
+		std::cout << "Did not find "<< search_m << std::endl;
+	else
+		std::cout << "Found: "<< it2->second << " for key: " << it2->first << std::endl;
+
+	// Multimap
+	std::cout << "--- Multi map ---" << std::endl;
+	std::multimap<int, int> mm;
+	mm.insert(std::pair<int, int>(21, 44));
+	int search_mm = 44;
+	std::multimap<int, int>::iterator it4 = easyfind(mm, search_mm);
+	if (it4 == mm.end())
+		std::cout << "Did not find "<< search_mm << std::endl;
+	else
+		std::cout << "Found: "<< it4->second << " for key: " << it4->first << std::endl;
+
+	// Multi Set
+	std::cout << "--- Multi Set  ---" << std::endl;
+	std::multiset<int> ms;
+	for (int i = 0; i < 10; i++)
+		ms.insert(i);
+	int search_ms = 42;
+	std::multiset<int>::iterator it5 = easyfind(ms, search_ms);
+	if (it5 == ms.end())
+		std::cout << "Did not find "<< search_ms << std::endl;
+	else
+		std::cout << "Found: " << *it5 << std::endl;
 }
