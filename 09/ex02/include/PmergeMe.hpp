910760301018d532e8863e706d9ceeea2156ffc4
Max Reidenbach <mreidenb@student.42heilbronn.de>
2024-05-12 19:29:31 +0200

    Added Make Pairs for Step one, mergePairs needs more understanding

--- 09/ex02/include/PmergeMe.hpp
+++ 09/ex02/include/PmergeMe.hpp
@@ -6,12 +6,13 @@
 /*   By: mreidenb <mreidenb@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 17:54:30 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/12 18:22:13 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/12 19:26:10 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <utility>
 #include <vector>
+#include <queue>
 
 class PmergeMe
 {
@@ -22,5 +23,7 @@ public:
 	PmergeMe &operator=(PmergeMe const &rhs);
 private:
 	void makePairs(std::vector<int> const &input);
+	void mergePairs();
 	std::vector<std::pair<int, int>> _data;
+	std::vector<int> _sorted;
 };
--- 09/ex02/src/PmergeMe.cpp
+++ 09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 17:54:28 by mreidenb          #+#    #+#             */
-/*   Updated: 2024/05/12 18:21:51 by mreidenb         ###   ########.fr       */
+/*   Updated: 2024/05/12 19:24:17 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,24 @@ PmergeMe &PmergeMe::operator=(PmergeMe const &rhs)
 
 void PmergeMe::makePairs(std::vector<int> const &input)
 {
-	
+	for (size_t i = 0; i < input.size(); i += 2)
+	{
+		if (i + 1 < input.size())
+		{
+			// Compare the two values and create a pair with the smaller value first
+			_data.push_back(std::make_pair(input[i] < input[i + 1] ? input[i] : input[i + 1], 
+				input[i] < input[i + 1] ? input[i + 1] : input[i])); 
+		}
+		// If there is only one value left, create a pair with the same value twice
+		else
+			_data.push_back(std::make_pair(input[i], input[i]));
+	}
+}
+
+void PmergeMe::mergePairs()
+{
+	std::priority_queue<std::pair<int, int>> pq;
+	for (int i = 0; i < _data.size(); ++i) {
+    	pq.push(std::make_pair(_data[i].second, i));  // _data[i].second is the larger element in the pair
+}
 }
